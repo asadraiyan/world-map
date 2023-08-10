bbcd1fcf6e6cae3c1796651324eee1690bbcd5d5
Asad Raiyan <asadraiyan001@gmail.com>
2023-08-10 13:02:39 +0530

    added search box

--- src/Components/WorldMap.js
+++ src/Components/WorldMap.js
@@ -7,6 +7,7 @@ const WorldMap = () => {
   console.log("mapData", mapData);
 
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [search, setSearch] = useState("");
 
   const onEachCountry = (country, layer) => {
     console.log("country =", country);
@@ -31,35 +32,97 @@ const WorldMap = () => {
     });
   };
 
+  const getCurrency = () => {
+    const currencyData = selectedCountry.currencies;
+    const currencyList = Object.keys(currencyData);
+    const currencyKey = currencyList[0];
+    const currencyName = currencyData[currencyKey].name;
+    const currencySymbol = currencyData[currencyKey].symbol;
+    return {
+      name: currencyName,
+      symbol: currencySymbol,
+    };
+  };
+  const getLanguage = () => {
+    const languagesData = selectedCountry.languages;
+    const languageList = Object.keys(languagesData);
+    const languageKey1 = languageList[0];
+    const languageKey2 = languageList[1];
+    const languageKey3 = languageList[2];
+    const languageName1 = languagesData[languageKey1];
+    const languageName2 = languagesData[languageKey2];
+    const languageName3 = languagesData[languageKey3];
+    return [languageName1, languageName2, languageName3];
+  };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <>
-      <MapContainer
-        center={[26.871653972198477, 80.88534341754298]}
-        zoom={2}
-        style={{ height: "80vh", width: "100vw" }}
-      >
-        <GeoJSON data={mapData.features} onEachFeature={onEachCountry} />
-      </MapContainer>
-      {selectedCountry && (
-        <div>
-          <h2>{selectedCountry.name.common}</h2>
-          <p>Capital: {selectedCountry.capital}</p>
-          <p>
-            Currency: {selectedCountry.currencies.INR?.symbol},{" "}
-            {selectedCountry.currencies.INR.name}
-          </p>
-          <p>Population: {selectedCountry.population}</p>
-          <p>Latlang: {selectedCountry.latlng}</p>
-          <p>
-            Laanguages: {selectedCountry.languages.eng},{" "}
-            {selectedCountry.languages.hin}, {selectedCountry.languages.tam}
-          </p>
-          <p>Area: {selectedCountry.area}</p>
-          <p>TimeZone: {selectedCountry.timezones}</p>
-          <p>Region: {selectedCountry.region}</p>
-          <img src={selectedCountry.flags[1]} alt="flag" />
+      <div className="main-container">
+        <input
+          type="search"
+          placeholder="Search"
+          className="search-box"
+          value={search}
+          onChange={handleSearch}
+        />
+        <div className="container">
+          <div className="map-container">
+            <MapContainer
+              center={[26.871653972198477, 80.88534341754298]}
+              zoom={2}
+              style={{ height: "80vh", width: "100%" }}
+            >
+              <GeoJSON data={mapData.features} onEachFeature={onEachCountry} />
+            </MapContainer>
+          </div>
+          {selectedCountry && (
+            <div className="countries">
+              <div className="country-flag">
+                <h2>{selectedCountry.name.common}</h2>
+                <img
+                  src={selectedCountry.flags[1]}
+                  className="flag-img"
+                  alt="flag"
+                />
+              </div>
+              <div className="country-data">
+                <p className="country-font">
+                  Capital: <span>{selectedCountry.capital}</span>
+                </p>
+                <p className="country-font">
+                  Currency:{" "}
+                  <span>
+                    {" "}
+                    {getCurrency()?.symbol}, {getCurrency()?.name}
+                  </span>
+                </p>
+                <p className="country-font">
+                  Population: <span>{selectedCountry.population}</span>
+                </p>
+                <p className="country-font">
+                  Latlang: <span>{selectedCountry.latlng}</span>
+                </p>
+                <p className="country-font">
+                  Laanguages: <span className="language">{getLanguage()}</span>
+                </p>
+                <p className="country-font">
+                  Area: <span>{selectedCountry.area}</span>
+                </p>
+                <p className="country-font">
+                  TimeZone: <span>{selectedCountry.timezones}</span>
+                </p>
+                <p className="country-font">
+                  Region: <span>{selectedCountry.region}</span>{" "}
+                </p>
+              </div>
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </>
   );
 };
